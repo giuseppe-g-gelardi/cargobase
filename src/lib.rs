0067d77a19e71fcd8007c241f4fa13856dc5ed4b
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-12-18 15:46:40 -0500

    start separate database operations -- io, util, queries, etc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod database;
 pub mod query;
 pub mod table;
 pub mod util;
@@ -8,12 +7,14 @@ pub mod columns;
 pub mod errors;
 pub mod row;
 
+pub mod database;
+pub use database::Database;
+
 pub use columns::{Column, Columns};
 pub use errors::DatabaseError;
 pub use row::Row;
 
-pub use database::Database;
-pub use query::Query;
+pub use query::{Operation, Query};
 pub use table::Table;
 pub use util::setup_temp_db;
 pub use view::View;
